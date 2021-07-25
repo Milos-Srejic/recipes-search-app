a568924ea8d2808b3547dac354967b26ca6de2e0
Milos Srejic <srejicmilos@hotmail.com>
2021-07-25 19:59:16 +0200

    Display searched recipes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ window.onload = () => {
   searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const term = document.getElementById('search-form-input').value;
-    console.log(term);
     getData(term);
   });
 
@@ -13,11 +12,55 @@ window.onload = () => {
     const api_url = `https://api.edamam.com/search?q=${term}&app_id=${app_id}&app_key=${app_key}`;
     try {
       let response = await fetch(api_url);
-      console.log(response);
+
       let data = await response.json();
-      console.log(data);
+
+      displayData(data);
     } catch (error) {
       console.error(error);
     }
+  };
+  const displayData = (data) => {
+    document.getElementById('recipes-box').textContent = '';
+    let recipes = Array.from(data.hits);
+    recipes.map((recipe) => {
+      console.log(recipe.recipe.ingredientLines);
+      const recipeCard = document.createElement('div');
+      const recipeImg = document.createElement('img');
+      const recipeTitle = document.createElement('h3');
+      const recipeBottom = document.createElement('div');
+      const recipeCalories = document.createElement('span');
+      const recipeIngredients = document.createElement('span');
+      const hiddenIngredients = document.createElement('div');
+
+      recipeCard.classList.add('recipe-card');
+      recipeBottom.classList.add('recipe-card-bottom');
+      recipeCalories.classList.add('recipe-calories');
+      recipeIngredients.classList.add('recipe-ingredients');
+      hiddenIngredients.classList.add('hidden-ingrediants');
+
+      recipeImg.src = recipe.recipe.image;
+      recipeTitle.innerText = recipe.recipe.label;
+      recipeCalories.innerText =
+        'Calories: ' + Math.round(recipe.recipe.calories);
+      recipeIngredients.textContent = 'Ingredients';
+      hiddenIngredients.innerText = recipe.recipe.inredientLines;
+      recipeBottom.appendChild(recipeCalories);
+      recipeBottom.appendChild(recipeIngredients);
+      recipeBottom.appendChild(hiddenIngredients);
+
+      recipeCard.appendChild(recipeImg);
+      recipeCard.appendChild(recipeTitle);
+      recipeCard.appendChild(recipeBottom);
+
+      document.getElementById('recipes-box').appendChild(recipeCard);
+
+      recipeCard.addEventListener('click', (e) => {
+        if (e.target.className === 'recipe-ingredients') {
+          console.log(e.target.nextSibling);
+          e.target.nextSibling.style.display = 'block';
+        }
+      });
+    });
   };
 };
